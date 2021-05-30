45710d4a6c33ce1779dfc822f4a378a6619f4201
cepheicephei <daniel.mccharen@yahoo.com>
2021-05-30 18:08:16 +0200

    fix height

--- src/sketch.js
+++ src/sketch.js
@@ -38,10 +38,10 @@ function setup() {
   // myWidth = floor(windowWidth / 100) * 100;
   // myHeight = floor(windowHeight / 100) * 100;
 
-  inititalizeGlobalVariables();
   let div = document.querySelector('#wrapper');
   myWidth = floor(div.clientWidth / 100) * 100;
   myHeight = floor(windowHeight / 100) * 100;
+  inititalizeGlobalVariables();
   // div.addClass('wrapper');
   createCanvas(myWidth, myHeight).parent(div).addClass('canvas');
 
